00b3206b7eaee0eaa719e76e71e86fc55d4e7b7e
fernandesdf <diogo99fernandes99@gmail.com>
2023-05-14 11:30:50 +0100

    clusteringRealities and TODO File
    Project's main file Clustering done until the step which uses the kmeans with initial centroids already predefined.
    Also made a TODO file.

---
+++ TODO.md
@@ -0,0 +1,22 @@
+# Project
+
+Project Description
+
+<em>[TODO.md spec & Kanban Board](https://bit.ly/3fCwKfM)</em>
+
+### Todo
+
+- [ ] STEP 5 -> WRITE OUTPUT FILE  
+- [ ] STEP 4 -> FIND CLUSTERS REAL CENTROIDS  
+
+### In Progress
+
+- [ ] example task  
+
+### Done ✓
+
+- [x] STEP 3 -> KMEANS (W/ INITIAL CENTROIDS)  
+- [x] STEP 2 -> MAKE CANDIDATES AND EXEMPLARS OBJECTS  
+- [x] STEP 1 -> READ FILES AND MAKE FEATURE VECTORS  
+- [x] STEP 0 -> ARGUMENTS FROM COMMAND LINE  
+
--- clusteringRealities.py
+++ clusteringRealities.py
@@ -1,14 +1,83 @@
-# from kMeansClustering import kmeans, trykmeans
+from kMeansClustering import kmeans, readTitlesFile, \
+    readCandidatesFile, translateToFeatureVector, \
+    Example, dissimilarity
+from CommandInputError import CommandInputError
+import sys
 
-# READ FILE
 
-# END OF READ FILE
+## STEP 0 ##
+# ARGUMENTS FROM COMMAND LINE
+k = int(sys.argv[1])
+titlesFileName = sys.argv[2]
+candidatesFileName = sys.argv[3]
+# END OF ARGUMENTS FROM COMMAND LINE
 
-# CREATE CAMDIDATES
 
+## STEP 1 ##
+# READ FILES
+scoresList, titlesList = readTitlesFile(titlesFileName)
+
+candidatesNames, candidatesTitles, \
+exemplarsName, exemplarsTitles \
+    = readCandidatesFile(candidatesFileName)
+# END OF READ FILES
+
+# TRANSLATE FEATURES STRINGS TO SCORES
+# Candidates feature vector
+candfeatureVectors = []
+for titles in candidatesTitles:
+    featureVector = translateToFeatureVector(scoresList, \
+                       titlesList, titles)
+    candfeatureVectors.append(featureVector)
+
+# Exemplars feature vector
+exemfeatureVectors = []
+for titles in exemplarsTitles:
+    featureVector = translateToFeatureVector(scoresList, \
+                       titlesList, titles)
+    exemfeatureVectors.append(featureVector)
+# END OF TRANSLATE FEATURES STRINGS TO SCORES
+
+
+## STEP 2 ##
+# CREATE CANDIDATES
+candidates = []
+for i in range(len(candidatesNames)): 
+    candidate = Example(candidatesNames[i], candfeatureVectors[i])
+    candidates.append(candidate)
 # END OF CREATE CANDIDATES
 
-# if examplars == "void":
-#     ... = trykmeans()
-# else:
-#     ... = kmeans()  
+# CREATE EXEMPLARS
+exemplars = []
+for i in range(len(exemplarsName)):
+    exemplar = Example(exemplarsName[i], exemfeatureVectors[i])
+    exemplars.append(exemplar)
+# END OF CREATE EXEMPLARS
+
+
+## STEP 3 ##
+# KMEANS (W/ INITIAL CENTROIDS)
+try:
+    if len(exemplars) != k:
+        raise CommandInputError
+
+    clusters = kmeans(candidates, exemplars, k, True)
+    print(str(dissimilarity(clusters)))
+except CommandInputError:
+    print("Command input and input file error: inconsistency " + \
+          "between k and number of initial centroids from " + \
+          "command line and file " + \
+          "{}, respectively".format(candidatesFileName))
+# END OF KMEANS
+
+
+## STEP 4 ##
+# FIND CLUSTERS REAL CENTROIDSP
+
+# END OF FIND CLUSTERS REAL CENTROIDS
+
+
+## STEP 5 ##
+# WRITE OUTPUT FILE
+
+# END OF WRITE OUTPUT FILE
